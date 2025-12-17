29b31ff5a0e98648361065fd2ab5edd7bb85ee71
Justin Snelling <Jus2210353@maricopa.edu>
2025-12-17 15:11:33 -0700

    Add OSHI library calls to display operating system and processor information

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,5 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println(new oshi.SystemInfo().getOperatingSystem());
+        System.out.println(new oshi.SystemInfo().getHardware().getProcessor());
     }
 }
