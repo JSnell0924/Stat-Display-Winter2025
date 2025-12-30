37e227d7b6cd919e993c04350b3168ab3385209f
JSnell0924 <Snellingj0924@gmail.com>
2025-12-29 18:33:27 -0700

    change cpuinfo to be more macOS friendly

--- app/src/main/java/org/example/Features/cpu/cpuInfo.java
+++ app/src/main/java/org/example/Features/cpu/cpuInfo.java
@@ -15,18 +15,34 @@ public class cpuInfo {
         CentralProcessor cpu = hw.getProcessor();
         CentralProcessor.ProcessorIdentifier processorIdentifier = cpu.getProcessorIdentifier();
 
+        long[][] prevTicks = cpu.getProcessorCpuLoadTicks();
+
         System.out.println("CPU Details");
         System.out.println("Processor Vendor: " + processorIdentifier.getVendor());
         System.out.println("Processor Name: " + processorIdentifier.getName());
         System.out.println("Processor ID: " + processorIdentifier.getProcessorID());
         System.out.println("Identifier: " + processorIdentifier.getIdentifier());
         System.out.println("Microarchitecture: " + processorIdentifier.getMicroarchitecture());
-        System.out.println("Frequency (Hz): " + processorIdentifier.getVendorFreq());
-        System.out.println("Frequency (GHz): " + processorIdentifier.getVendorFreq() / 1000000000.0);
+
         System.out.println("Number of physical packages: " + cpu.getPhysicalPackageCount());
         System.out.println("Number of physical CPUs: " + cpu.getPhysicalProcessorCount());
         System.out.println("Number of logical CPUs: " + cpu.getLogicalProcessorCount());
         System.out.printf("Total CPU usage: %.2f%%%n", cpu.getSystemCpuLoad(1000) * 100);
+        
+        double[] load = cpu.getProcessorCpuLoadBetweenTicks(prevTicks);
+        long[] freqs = cpu.getCurrentFreq();
+
+       for (int i = 0; i < load.length; i++) {
+            double usagePercent = load[i] * 100;
+            String freqStr;
+
+            if (freqs != null && i < freqs.length && freqs[i] > 0) {
+                freqStr = String.format("%.2f GHz", freqs[i] / 1_000_000_000.0);
+            } else {
+                freqStr = "N/A";
+            }
+            System.out.printf("CPU Core %d usage: %.2f%%, frequency: %s%n", i, usagePercent, freqStr);
+        }
         System.out.println("End CPU Details");
     }
 }
