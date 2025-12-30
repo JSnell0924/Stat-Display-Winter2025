4fe762b479ac2ab8215b0e447425e44961ce05da
Luka-csc <Lukapowers64@gmail.com>
2025-12-29 17:33:25 -0700

    Getting Name for Storage Device

--- app/src/main/java/org/example/Features/disk/DiskInfo.java
+++ app/src/main/java/org/example/Features/disk/DiskInfo.java
@@ -9,20 +9,27 @@ import oshi.hardware.Display;
 import oshi.hardware.HWDiskStore;
 import oshi.hardware.HardwareAbstractionLayer;
 import oshi.hardware.Sensors;
+import oshi.util.tuples.Pair;
 
 public class DiskInfo {
     public static void getDiskInfo() {
+        
         SystemInfo si = new SystemInfo();
         HardwareAbstractionLayer hw = si.getHardware();
         Sensors sensors = hw.getSensors();
-        //double cpuTemp = sensors.getCpuTemperature();
-        //System.out.println(cpuTemp);
-        //List<Display> gpuTemp = hw.getDisplays();
-        //System.out.println(gpuTemp);
-
         List<HWDiskStore> diskStorage = hw.getDiskStores();
-        System.out.println(diskStorage);
+        //Pair<Integer, Integer> dsPartIdx = getDiskStoreAndPartitionForPath(filePath, diskStores);
+
+        for (HWDiskStore disk : diskStorage) {
+            System.out.println("Name: " + disk.getModel());
+        }
 
         System.out.println("PushPullTest");
     }
 }
+
+
+ //double cpuTemp = sensors.getCpuTemperature();
+        //System.out.println(cpuTemp);
+        //List<Display> gpuTemp = hw.getDisplays();
+        //System.out.println(gpuTemp);
